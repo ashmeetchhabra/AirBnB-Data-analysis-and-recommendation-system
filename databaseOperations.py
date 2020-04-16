86c2bea3eed05843f906203a4c2526f790830965
Ashmeet <ash.kaurcw@gmail.com>
2020-04-16 03:41:45 -0400

    databaseOperations.py file modified with flask basic functionality for displaying data and index.html file added with only displaying data

--- databaseOperations.py
+++ databaseOperations.py
@@ -1,5 +1,9 @@
 import pymongo
 from pprint import pprint
+from flask import Flask, render_template, request, redirect, url_for
+
+
+app = Flask(__name__)
 
 connection = pymongo.MongoClient('localhost', 27017)
 database = connection['Airbnb']
@@ -14,3 +18,13 @@ retrieve_data = collection.find({"neighbourhood_group": neighbourhood_group})
 
 for document in retrieve_data:
     pprint(document)
+
+
+@app.route("/")
+def display():
+    data_test = collection.find({"host_name": "Ben"})
+    return render_template('index.html', t="Testing", data=list(data_test), h="Airbnb  Data")
+
+
+if __name__ == "__main__":
+    app.run()
