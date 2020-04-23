7a7e5b67def769a6a8ac91bb91b00260980e98d1
Ashmeet <ash.kaurcw@gmail.com>
2020-04-23 00:18:35 -0400

    databaseOperations.py file modified with complete basic query 3.

--- databaseOperations.py
+++ databaseOperations.py
@@ -1,7 +1,6 @@
 import pymongo
 from pprint import pprint
-from flask import Flask, render_template, request, redirect, url_for
-
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -19,11 +18,36 @@ retrieve_data = collection.find({"neighbourhood_group": neighbourhood_group})
 for document in retrieve_data:
     pprint(document)
 
+# Basic Query 3
+listing_id = input("Enter the listing id to which you want to provide reviews.")
+review = int(input("Give the reviews (out of 5) to the listing: "))
+if review < 0 or review > 5:
+    print("Invalid Review.")
+else:
+    given_query = {"id": listing_id}
+    num_of_reviews = collection.find({"id": listing_id}, {"number_of_reviews": 1})
+    num_of_reviews_1 = list(num_of_reviews)[0]["number_of_reviews"]
+    print("currently num_of_reviews are:", int(num_of_reviews_1))
+    review_per_month = (num_of_reviews_1 + 1) / 120
+    update_query = {
+        "$set": {"reviews": review, "number_of_reviews": int(num_of_reviews_1) + 1,
+                 "reviews_per_month": review_per_month},
+        "$currentDate": {"last_review": True}
+    }
+    print("Correct Review Given")
+    collection.update_one(given_query, update_query)
+
+
+# Sophisticated Query 2
+
+# Sophisticated Query 4
+
+
 
 @app.route("/")
 def display():
     data_test = collection.find({"host_name": "Ben"})
-    return render_template('index.html', t="Testing", data=list(data_test), h="Airbnb  Data")
+    return render_template('index.html', t="Testing", data=list(data_test), h="Airbnb Data")
 
 
 if __name__ == "__main__":
